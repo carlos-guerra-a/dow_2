5af97b52e7dfdee17f9076904d58f5c305e4880a
FabianRomanUSM <fabian.roman@usm.cl>
2023-06-28 21:23:15 -0400

    Diseño
    Diseño de index home
    
    Co-Authored-By: Carlos Guerra <135287095+carlos-guerra-a@users.noreply.github.com>

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,57 +1,135 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-  /* Style the input field */
-  #myInput {
-    padding: 20px;
-    margin-top: -6px;
-    border: 0;
-    border-radius: 0;
-    background: #f1f1f1;
-  }
-  </style>
-</head>
-<body>
+@extends('templates.master')
 
-  <div class="container">
-    <h2>Galería de Artistas</h2>
-    <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Artistas
-        <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-            <input class="form-control" id="myInput" type="text" placeholder="Search..">
-            @foreach ($cuentas as $cuenta)
-                @if ($cuenta->perfil_id !== 1)
-                    <li>
-                        <a href="{{ route('artista.home', $cuenta->user) }}" class="btn btn-link">{{ $cuenta->nombre }} {{ $cuenta->apellido }}</a>
-                    </li>
-                @endif
-            @endforeach
-        </ul>
-    </div>
+@section('contenido')
+<style>
+  * {
+	margin: 0; 
+	padding: 0;
+}
+body {
+	background: #ccc; 
+	font-family: arial, verdana, tahoma;
+}
 
-    <div>
-      <a href="{{ route('login') }}" class="btn btn-primary ">Iniciar Sesión</a>
-      <a href="{{ route('crear.cuenta') }}" class="btn btn-primary">Crear cuenta de artista</a>
-    </div>
-  </div>
+.artist {
+  margin: 20px auto;
+
+}
+
+.accordian {
+	width: 805px; height: 320px;
+	overflow: hidden;
+	
+	/* Tiempo para algunos estilos */
+	margin: 50px auto;
+	box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.35);
+	-webkit-box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.35);
+	-moz-box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.35);
+}
+
+/* Un pequeño truco para evitar parpadeos en algunos navegadores */
+.accordian ul {
+	width: 2000px;
+	/* Esto dará suficiente espacio al último elemento para moverse
+	en lugar de caerse/parpadear durante los hover */
+}
+
+.accordian li {
+	position: relative;
+	display: block;
+	width: 160px;
+	float: left;
+	
+	border-left: 1px solid #888;
+	
+	box-shadow: 0 0 25px 10px rgba(0, 0, 0, 0.5);
+	-webkit-box-shadow: 0 0 25px 10px rgba(0, 0, 0, 0.5);
+	-moz-box-shadow: 0 0 25px 10px rgba(0, 0, 0, 0.5);
+	
+	/* Transiciones para dar efecto de animación */
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+	/* Si te desplazas sobre las imágenes ahora, deberías poder ver el acordeón básico */
+}
+
+/* Reduce el ancho de los elementos sin hover */
+.accordian ul:hover li {width: 40px;}
+/* Aplica efectos de hover ahora */
+/* Los estilos de hover de LI deben anular los estilos de hover de UL */
+.accordian ul li:hover {width: 640px;}
 
 
-<script>
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".dropdown-menu li").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-</script>
+.accordian li img {
+	display: block;
+}
 
-</body>
-</html>
+.gallery {
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    .gallery img {
+      width: 805px; /* Establece el ancho deseado */
+      height: 320px; /* Establece la altura deseada */
+      object-fit: cover; /* Ajusta la imagen para llenar el espacio sin deformarla */
+      margin: 5px; /* Agrega un margen entre las imágenes */
+    }
+</style>
+
+<div class="dropdown artist">
+  <button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown">Buscar Artistas</button>
+  <ul class="dropdown-menu">
+      <input class="form-control" id="myInput" type="text" placeholder="Buscar">
+      @foreach ($cuentas as $cuenta)
+          @if ($cuenta->perfil_id !== 1)
+              <li>
+                  <a href="{{ route('artista.home', $cuenta->user) }}" class="btn btn-link">{{ $cuenta->nombre }} {{ $cuenta->apellido }}</a>
+              </li>
+          @endif
+      @endforeach
+  </ul>
+</div>
+
+<div class="accordian gallery">
+	<ul>
+		<li>
+      <img src="{{asset('images/imagen_01.jpg')}}">
+		</li>
+		<li>
+      <img src="{{asset('images/imagen_02.jpg')}}">
+		</li>
+		<li>			
+			<img src="{{asset('images/imagen_03.jpg')}}">	
+		</li>
+		<li>
+			<img src="{{asset('images/imagen_04.jpg')}}">
+		</li>
+		<li>
+			<img src="{{asset('images/imagen_05.jpg')}}">
+		</li>
+	</ul>
+</div>
+
+<div class="accordian gallery">
+	<ul>
+		<li>
+      <img src="{{asset('images/imagen_06.jpg')}}">
+		</li>
+		<li>
+      <img src="{{asset('images/imagen_07.jpg')}}">
+		</li>
+		<li>			
+			<img src="{{asset('images/imagen_08.jpg')}}">	
+		</li>
+		<li>
+			<img src="{{asset('images/imagen_09.jpg')}}">
+		</li>
+    <li>
+			<img src="{{asset('images/imagen_10.png')}}">
+		</li>
+	</ul>
+</div>
+
+@endsection
+
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -1,19 +1,48 @@
 <!doctype html>
 <html lang="es">
 <head>
- {{-- Holi --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-
     <title>@yield('title')</title>
 </head>
 <body>
+
+    {{-- Barra de usuario --}}
+    <div class="container-fluid">
+        <div class="row bg-dark text-white">
+            <div class="col-8">
+                Bienvenido Nombre_perfil Nombre_usuario Apellido_usuario
+                {{-- Bienvenido <span class="fw-bold">{{Auth::cuenta()->nombre}} </span>[{{Auth::perfil()->nombre}}] --}}
+            </div>
+            <div class="col-2 text-end d-none d-lg-block">
+                {{-- Último inicio de sesión: 01/04/2023 a las 18:34
+                Último inicio de sesión: {{date('d-m-Y',strtotime(Auth::user()->ultimo_login))}} a las {{date('H:i',strtotime(Auth::user()->ultimo_login))}} --}}
+            </div>
+            <div class="col-2 text-end d-none d-lg-block">
+                {{-- href="{{route('perfiles.logout')}}" --}}
+                <a href="#" class="text-white">Cerrar Sesión</a>
+            </div>
+        </div>
+    </div>
+
+    {{-- Navbar --}}  
+        <nav class="navbar bg-body-tertiary" style="background-color: #f1e7b0;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('index') }}">
+                    <img src="{{asset('images/logo.ico')}}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    PhotoArt
+                </a>
+                <div>
+                    <a href="{{ route('login') }}" class="btn btn-primary me-2">Iniciar Sesión</a>
+                    <a href="{{ route('crear.cuenta') }}" class="btn btn-success">Crear cuenta de artista</a>
+                </div>
+            </div>
+        </nav>
+        
+    
+    {{-- Contenido principal --}}
     <div class="container">
         @yield('contenido')
     </div>
@@ -23,5 +52,15 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#myInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $(".dropdown-menu li").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
 </body>
 </html>
